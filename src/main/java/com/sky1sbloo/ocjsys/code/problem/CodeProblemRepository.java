7de79934b02be809bf5223259526fbab573204ea
Sky1sBloo <ultimate8gaming@gmail.com>
2026-03-04 10:30:24 +0800

    Added code problem repository

---
+++ src/main/java/com/sky1sbloo/ocjsys/code/problem/CodeProblemRepository.java
@@ -0,0 +1,15 @@
+package com.sky1sbloo.ocjsys.code.problem;
+
+import com.sky1sbloo.ocjsys.userprofile.UserProfile;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface CodeProblemRepository extends JpaRepository<CodeProblem, Long> {
+    List<CodeProblem> findByUserProfile(UserProfile userProfile);
+    List<CodeProblem> findByTitleIgnoreCaseContaining(String title);
+    List<CodeProblem> findByDifficulty(Difficulties difficulty);
+    List<CodeProblem> findByTagsContaining(String tag);
+}
