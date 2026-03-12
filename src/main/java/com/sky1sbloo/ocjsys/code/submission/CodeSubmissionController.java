0bb3635082c4b4109f6c0fe238265f4d899d661c
Sky1sBloo <ultimate8gaming@gmail.com>
2026-03-12 21:51:10 +0800

    Made controller return created http status

--- src/main/java/com/sky1sbloo/ocjsys/code/submission/CodeSubmissionController.java
+++ src/main/java/com/sky1sbloo/ocjsys/code/submission/CodeSubmissionController.java
@@ -4,6 +4,7 @@ import com.sky1sbloo.ocjsys.auth.AuthUser;
 import com.sky1sbloo.ocjsys.code.submission.dto.CodeSubmissionDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
@@ -30,7 +31,7 @@ public class CodeSubmissionController {
             Thread.currentThread().interrupt();
             return ResponseEntity.badRequest().body("Error executing code");
         }
-        return ResponseEntity.ok().body("Code submitted successfully");
+        return ResponseEntity.status(HttpStatus.CREATED).body("Code submitted successfully");
     }
 
     @PostMapping("/run")
