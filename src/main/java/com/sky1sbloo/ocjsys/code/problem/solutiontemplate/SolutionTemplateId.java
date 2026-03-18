62be100ec2572c8375ebb9520b9f2ea9aa5d108c
Sky1sBloo <ultimate8gaming@gmail.com>
2026-03-18 10:40:59 +0800

    Finished solution tempalte service

--- src/main/java/com/sky1sbloo/ocjsys/code/problem/solutiontemplate/SolutionTemplateId.java
+++ src/main/java/com/sky1sbloo/ocjsys/code/problem/solutiontemplate/SolutionTemplateId.java
@@ -2,9 +2,15 @@ package com.sky1sbloo.ocjsys.code.problem.solutiontemplate;
 
 import com.sky1sbloo.ocjsys.code.CodeLanguage;
 import com.sky1sbloo.ocjsys.code.problem.CodeProblem;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
+@AllArgsConstructor
+@NoArgsConstructor
+@Builder
 public class SolutionTemplateId implements Serializable {
     private CodeProblem codeProblem;
     private CodeLanguage language;
--- src/main/java/com/sky1sbloo/ocjsys/code/problem/solutiontemplate/SolutionTemplateService.java
+++ src/main/java/com/sky1sbloo/ocjsys/code/problem/solutiontemplate/SolutionTemplateService.java
@@ -1,6 +1,10 @@
 package com.sky1sbloo.ocjsys.code.problem.solutiontemplate;
 
+import com.sky1sbloo.ocjsys.code.CodeLanguage;
+import com.sky1sbloo.ocjsys.code.problem.CodeProblem;
 import com.sky1sbloo.ocjsys.code.problem.CodeProblemRepository;
+import com.sky1sbloo.ocjsys.code.problem.solutiontemplate.dto.SolutionTemplateDto;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
@@ -10,5 +14,43 @@ public class SolutionTemplateService {
     private final CodeProblemRepository codeProblemRepository;
     private final SolutionTemplateRepository solutionTemplateRepository;
 
-    public void addCodeProblemInitial
+    public void addSolutionTemplate(SolutionTemplateDto solutionTemplateDto) {
+        SolutionTemplate solutionTemplate = createSolutionTemplateFromDto(solutionTemplateDto);
+        solutionTemplateRepository.save(solutionTemplate);
+    }
+
+    public void editSolutionTemplate(SolutionTemplateDto solutionTemplateDto) {
+        SolutionTemplateId templateId = createTemplateIdFromDto(solutionTemplateDto);
+        solutionTemplateRepository.deleteById(templateId);
+        SolutionTemplate solutionTemplate = createSolutionTemplateFromDto(solutionTemplateDto);
+        solutionTemplateRepository.save(solutionTemplate);
+    }
+
+    public void deleteSolutionTemplate(SolutionTemplateDto solutionTemplateDto) {
+        SolutionTemplateId templateId = createTemplateIdFromDto(solutionTemplateDto);
+        solutionTemplateRepository.deleteById(templateId);
+    }
+
+    private SolutionTemplate createSolutionTemplateFromDto(SolutionTemplateDto solutionTemplateDto)
+            throws EntityNotFoundException, IllegalArgumentException {
+        CodeProblem problem = codeProblemRepository.findById(solutionTemplateDto.problemId()).orElseThrow(
+                () -> new EntityNotFoundException("Code problem with id: " + solutionTemplateDto.problemId() +
+                        " not found")
+        );
+        var language = CodeLanguage.valueOf(solutionTemplateDto.language());
+        return SolutionTemplate.builder()
+                .codeProblem(problem)
+                .language(language)
+                .sourceCode(solutionTemplateDto.sourceCode()).build();
+    }
+
+    private SolutionTemplateId createTemplateIdFromDto(SolutionTemplateDto solutionTemplateDto)
+            throws EntityNotFoundException, IllegalArgumentException {
+        CodeProblem problem = codeProblemRepository.findById(solutionTemplateDto.problemId()).orElseThrow(
+                () -> new EntityNotFoundException("Code problem with id: " + solutionTemplateDto.problemId() +
+                        " not found")
+        );
+        var language = CodeLanguage.valueOf(solutionTemplateDto.language());
+        return new SolutionTemplateId(problem, language);
+    }
 }
