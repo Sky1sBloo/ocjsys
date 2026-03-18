d9c70b4c2a95ef48585d5bbc34283b421bd6aee9
Sky1sBloo <ultimate8gaming@gmail.com>
2026-03-18 15:02:57 +0800

    Made verifier dto not return code problem again

--- src/main/java/com/sky1sbloo/ocjsys/code/problem/verifier/dto/CodeProblemVerifierDto.java
+++ src/main/java/com/sky1sbloo/ocjsys/code/problem/verifier/dto/CodeProblemVerifierDto.java
@@ -1,7 +1,6 @@
 package com.sky1sbloo.ocjsys.code.problem.verifier.dto;
 
 import com.sky1sbloo.ocjsys.code.CodeLanguage;
-import com.sky1sbloo.ocjsys.code.problem.dto.CodeProblemDto;
 import com.sky1sbloo.ocjsys.code.problem.verifier.CodeProblemVerifier;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -14,13 +13,11 @@ import lombok.NoArgsConstructor;
 @Builder
 public class CodeProblemVerifierDto {
     private Long id;
-    private CodeProblemDto problem;
     CodeLanguage language;
     String sourceCode;
 
     public CodeProblemVerifierDto(CodeProblemVerifier verifier) {
         this.id = verifier.getId();
-        this.problem = new CodeProblemDto(verifier.getProblem());
         this.language = verifier.getLanguage();
         this.sourceCode = verifier.getSourceCode();
     }
