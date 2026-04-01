7e271184976c13f680fa0040f78936e0ac48e8aa
Sky1sBloo <ultimate8gaming@gmail.com>
2026-04-01 11:06:38 +0800

    Removed unused dependencies

--- src/main/java/com/sky1sbloo/ocjsys/code/submission/CodeSubmissionController.java
+++ src/main/java/com/sky1sbloo/ocjsys/code/submission/CodeSubmissionController.java
@@ -4,14 +4,12 @@ import com.sky1sbloo.ocjsys.auth.AuthUser;
 import com.sky1sbloo.ocjsys.code.submission.dto.CodeSubmissionDto;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 
-import java.io.IOException;
 import java.net.URI;
 
 @RequiredArgsConstructor
